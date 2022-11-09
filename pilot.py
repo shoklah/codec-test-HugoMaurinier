75a2980a6f60113a7b72de294ed5fab460d7245b
Hugo Maurinier <hugo.maurinier@epitech.eu>
2022-11-09 10:17:28 +0000

    [FIX] pilot code structure

--- pilot.py
+++ pilot.py
@@ -2,16 +2,20 @@
 from robot.robot import robot
 from sys import argv
 
-def main():
+def main(argv: list):
     if len(argv) != 3:
         print(f'''Usage: python3 pilot.py <map_dimensions> <command>
         ex: python3 pilot.py 5x5 FFRFLFLF''')
-        exit(1)
+        return 1
     map_dimensions = argv[1].split('x')
+    if len(map_dimensions) != 2:
+        print(f'''Usage: python3 pilot.py <map_dimensions> <command>
+        ex: python3 pilot.py 5x5 FFRFLFLF''')
+        return 1
     robot1 = robot(int(map_dimensions[0]), int(map_dimensions[1]))
     robot1.move(argv[2])
     robot1.print_position()
-    exit(0)
+    return 0
 
 if __name__ == "__main__":
-    main()
+    exit(main(argv))
